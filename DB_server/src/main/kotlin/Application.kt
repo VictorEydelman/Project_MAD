10db0ca5acfe08f751c4d5083e8b3160392a7a2e
Viktor1 <DoraGotova@gmail.com>
2025-03-17 20:50:16 +0300

    Commit Create DB_serve

---
+++ DB_server/src/main/kotlin/Application.kt
@@ -0,0 +1,13 @@
+package mad.project
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.tomcat.jakarta.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
