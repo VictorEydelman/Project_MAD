282ff84f7a18d6f3a9d32864c7239b5443b4c1bf
edshPC <ed_shh@mail.ru>
2025-03-22 00:05:04 +0300

    Init ktor server

---
+++ entry_microservice/src/main/kotlin/Routing.kt
@@ -0,0 +1,20 @@
+package ru.itmo
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.fasterxml.jackson.databind.*
+import io.ktor.serialization.jackson.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
