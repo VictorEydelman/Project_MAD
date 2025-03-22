2539fcb0a8984b1a07a524dd98fe50574d090f87
Viktor1 <DoraGotova@gmail.com>
2025-03-22 12:15:53 +0300

    Commit Create docker-compose.yaml

--- DB_server/src/main/kotlin/Application.kt
+++ DB_server/src/main/kotlin/Application.kt
@@ -12,7 +12,9 @@ fun Application.module() {
     install(ContentNegotiation) {
         json()
     }
+    println("r")
+    //docker run --name postgres-container --network my_network -e POSTGRES_USER=s291485 -e POSTGRES_PASSWORD=qwe -e POSTGRES_DB=MAD -p 5432:5432 -d postgres
     configureSerialization()
-    //configureDatabases()
+    configureDatabases()
     configureRouting()
 }
