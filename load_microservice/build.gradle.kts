67503a7f43c44a6b7bdf48ad97e76aa39bba18c0
Viktor1 <DoraGotova@gmail.com>
2025-03-29 12:16:18 +0300

    Docker all modules

--- load_microservice/build.gradle.kts
+++ load_microservice/build.gradle.kts
@@ -19,3 +19,10 @@ tasks.test {
 kotlin {
     jvmToolchain(17)
 }
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "ru.itmo.MainKt"
+    }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
