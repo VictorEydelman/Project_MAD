127f2399995c976af9a660f7a9ef34084263e2a4
Marat <marat.safin.2002@mail.ru>
2025-03-29 16:51:25 +0300

    base tests

--- load_microservice/build.gradle.kts
+++ load_microservice/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     kotlin("jvm") version "2.1.10"
+    kotlin("plugin.serialization") version "1.9.0"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "ru.itmo"
@@ -10,7 +13,20 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+
+    implementation("io.ktor:ktor-server-core-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.4")
+    implementation("io.ktor:ktor-client-core-jvm:2.3.4")
+    implementation("io.ktor:ktor-client-cio-jvm:2.3.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+    implementation("ch.qos.logback:logback-classic:1.4.8")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.ktor:ktor-server-tests-jvm:2.3.4")
+    testImplementation("io.ktor:ktor-client-mock-jvm:2.3.4")
 }
 
 tasks.test {
@@ -20,9 +36,15 @@ kotlin {
     jvmToolchain(17)
 }
 
-tasks.withType<Jar> {
+application {
+    mainClass.set("ru.itmo.MainKt")
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("load_microservice")
+    archiveClassifier.set("")
+    archiveVersion.set("")
     manifest {
         attributes["Main-Class"] = "ru.itmo.MainKt"
     }
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
