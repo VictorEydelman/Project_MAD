c7f4506a091b67ea46500856ec390cffa706d65f
VictorEydelman <113546427+VictorEydelman@users.noreply.github.com>
2025-03-30 13:41:47 +0300

    DB setting fix

--- DB_server/src/main/kotlin/Service/Alarm.kt
+++ DB_server/src/main/kotlin/Service/Alarm.kt
@@ -49,7 +49,6 @@ class AlarmService(private val connection: Connection){
             statement.setObject(2, settings.gender)
             statement.setObject(3,settings.physicalCondition)
             statement.setObject(4,settings.caffeineUsage)
-            statement.setObject(5,settings.alcoholUsage)
             statement.executeUpdate()
         } catch (e: SQLException){
             throw Exception("Ошибка с бд")
