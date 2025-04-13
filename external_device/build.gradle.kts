9aeeed249b942bcfc0cb070c9940bf25165fe690
ushpineva <ushpineva@ozon.ru>
2025-04-13 16:39:18 +0300

    docker parameters

--- external_device/build.gradle.kts
+++ external_device/build.gradle.kts
@@ -1,16 +1,14 @@
-val kotlin_version: String by project
-val logback_version: String by project
-
 plugins {
     kotlin("jvm") version "2.1.20"
-    id("io.ktor.plugin") version "3.1.1"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-group = "ru.itmo"
+group = "org.example"
 version = "0.1"
 
 application {
-    mainClass.set("ru.itmo.MainKt")
+    mainClass.set("org.example.MainKt")
 }
 
 kotlin {
@@ -28,6 +26,15 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation:2.3.5")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.5")
     implementation("ch.qos.logback:logback-classic:1.4.11")
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("external_device")
+    archiveClassifier.set("")
+    archiveVersion.set("")
+    manifest {
+        attributes["Main-Class"] = "org.example.MainKt"
+    }
 }
 
 tasks.test {
