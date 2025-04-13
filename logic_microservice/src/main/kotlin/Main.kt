3054ba2ca3eb86476470c9d633692146487ac77f
aulouu <alina.minkovva@gmail.com>
2025-04-13 22:08:39 +0300

    logic logger

--- logic_microservice/src/main/kotlin/Main.kt
+++ logic_microservice/src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
 package ru.itmo
 
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.slf4j.event.Level
+
 fun main() {
-    println("Hello World!")
+    embeddedServer(Netty, port = 8900, module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/api") }
+    }
+
+    routing {
+
+    }
 }
