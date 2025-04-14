8aafd6fc1e4cea8fefaf81f1266c422b3983bb10
nikitarigun <vozmozhno.rigun@yandex.ru>
2025-04-14 16:03:55 +0300

    UI files with MainScreen

--- SleepMonitor/app/src/main/java/mad/project/SleepMonitor/screens/MainScreen.kt
+++ SleepMonitor/app/src/main/java/mad/project/SleepMonitor/screens/MainScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-
 @Composable
 fun MainScreen(navController: NavController) {
     Surface(
