6e355413b53bff3d6c6b888f8c5821662b9ea379
edshPC <ed_shh@mail.ru>
2025-04-23 21:09:20 +0300

    Docs change

--- entry_microservice/src/main/kotlin/keydb/KeyDBClient.kt
+++ entry_microservice/src/main/kotlin/keydb/KeyDBClient.kt
@@ -21,9 +21,7 @@ class KeyDBClient(
     poolConfig: JedisPoolConfig = defaultPoolConfig()
 ) {
     private val pool = JedisPool(poolConfig, host, port)
-    private val objectMapper = jacksonObjectMapper().apply {
-        registerModules(JavaTimeModule())
-    }
+    private val objectMapper = jacksonObjectMapper().registerModule(JavaTimeModule())
 
     companion object {
         private fun defaultPoolConfig(): JedisPoolConfig {
