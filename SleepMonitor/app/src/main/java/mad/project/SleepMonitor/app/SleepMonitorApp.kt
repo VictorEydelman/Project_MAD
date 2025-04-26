cfc65d0308caf87dd9cf78622e4aafd24405eba1
GlankS21 <asgar21502@gmail.com>
2025-04-26 17:26:49 +0300

    Add: Alarm, Bed Time screens

--- SleepMonitor/app/src/main/java/mad/project/SleepMonitor/app/SleepMonitorApp.kt
+++ SleepMonitor/app/src/main/java/mad/project/SleepMonitor/app/SleepMonitorApp.kt
@@ -4,13 +4,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import mad.project.SleepMonitor.components.NormalTextComponent
 import mad.project.SleepMonitor.navigation.SleepMonitorNavigation
+import mad.project.SleepMonitor.notification.NotificationService
 
 @Composable
-fun SleepMonitorApp() {
+fun SleepMonitorApp(notificationService: NotificationService) {
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
-        SleepMonitorNavigation()
+        SleepMonitorNavigation(notificationService)
     }
 }
