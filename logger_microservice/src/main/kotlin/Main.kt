2c54a69cabaaabf59c1d83712ef06befd1730cb6
edshPC <ed_shh@mail.ru>
2025-04-27 22:59:00 +0300

    Logger implementation

--- logger_microservice/src/main/kotlin/Main.kt
+++ logger_microservice/src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
 package ru.itmo
 
-fun main() {
-    println("Hello World!")
+import KeyDBClient
+import LoggerRequestDTO
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val keydb = KeyDBClient()
+
+    Logging.info("Start listening for logs...")
+
+    keydb.subscribe("log", LoggerRequestDTO::class.java, true) {
+        Logging.handle(it)
+    }
+
+    Logging.info("Stopped listening for logs")
 }
