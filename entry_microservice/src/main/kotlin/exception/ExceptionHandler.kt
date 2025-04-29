0ce00d7fcc964850cc8e41b355f62e91151c9640
edshPC <ed_shh@mail.ru>
2025-04-29 19:31:58 +0300

    Entry logs

--- entry_microservice/src/main/kotlin/exception/ExceptionHandler.kt
+++ entry_microservice/src/main/kotlin/exception/ExceptionHandler.kt
@@ -5,17 +5,20 @@ import io.ktor.server.plugins.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import ru.itmo.dto.api.SimpleResponse
+import ru.itmo.keydb.Logger
 
 fun StatusPagesConfig.exceptionHandler() {
     exception<StatusException> { call, cause ->
         call.respond(cause.statusCode, SimpleResponse.failure(cause.message))
+        Logger.warn("User ${cause.statusCode.description}: ${cause.message}")
     }
     exception<BadRequestException> { call, cause ->
         call.respond(HttpStatusCode.BadRequest, SimpleResponse.failure(cause.message))
+        Logger.warn("User BadRequest: ${cause.message}")
     }
     exception<Throwable> { call, cause ->
         call.respond(HttpStatusCode.InternalServerError, SimpleResponse.failure(cause.message))
-        cause.printStackTrace()
+        Logger.error("Unexpected exception", cause)
     }
 
     status(
@@ -24,5 +27,6 @@ fun StatusPagesConfig.exceptionHandler() {
         HttpStatusCode.UnsupportedMediaType,
     ) { call, status ->
         call.respond(status, SimpleResponse.failure(status.description))
+        Logger.warn("User ${status.description}")
     }
 }
