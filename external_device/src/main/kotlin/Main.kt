17154594b1384cb3be7b00dc931bae92c6b094e6
edshPC <ed_shh@mail.ru>
2025-04-30 14:24:57 +0300

    External device fix 406

--- external_device/src/main/kotlin/Main.kt
+++ external_device/src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
 package org.example
 
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.serialization.jackson.jackson
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.routing.*
 import org.example.controller.HealthDataController
 import org.example.service.HealthDataService
@@ -13,6 +16,11 @@ fun main() {
 }
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
     val service = HealthDataService()
     val controller = HealthDataController(service)
     routing {
--- external_device/src/main/kotlin/dto/HealthDataDto.kt
+++ external_device/src/main/kotlin/dto/HealthDataDto.kt
@@ -1,10 +1,7 @@
 package org.example.dto
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class HealthDataDto(
         val timestamp: String,
-        val heartRate: Int,
+        val pulse: Int,
         val sleepPhase: String
 )
