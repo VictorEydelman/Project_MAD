cbdf9c4eb7fd9130805662ca368faf84651a8a64
VictorEydelman <113546427+VictorEydelman@users.noreply.github.com>
2025-04-30 14:36:49 +0300

    external device fix

--- external_device/src/main/kotlin/controller/HealthDataController.kt
+++ external_device/src/main/kotlin/controller/HealthDataController.kt
@@ -1,5 +1,6 @@
 package org.example.controller
 
+import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
